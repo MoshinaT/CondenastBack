cc3f21a6c43765c17e942b72b2e67eda76ad7e82
Moshina Thajudeen <moshina@byteblanket.com>
2021-06-09 19:38:03 +0530

    First commit

---
+++ accounts/account.service.js
@@ -0,0 +1,26 @@
+﻿const config = require('config.json');
+const axios = require("axios");
+
+module.exports = {
+    getAll,
+    getByVal,
+};
+
+
+async function getAll() {
+    console.log("fun");
+    let response = await axios.get(
+        ` https://newsapi.org/v2/top-headlines?country=gb&category=general&apiKey=${config.APIKey}`
+      );
+     console.log(response.data);
+    return response.data;
+}
+
+async function getByVal(val) {
+    let response = await axios.get(
+        `https://newsapi.org/v2/everything?q=${val}&sortBy=publishedAt&apiKey=${config.APIKey}`
+      );
+      console.log("ok")
+    return response.data;
+}
+
